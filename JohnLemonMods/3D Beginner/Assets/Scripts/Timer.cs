24f23b451efee75b4a69551f22867b40f96130e1
Elliot Craig <ellcraig@iu.edu>
2021-03-29 20:40:24 -0400

    Added Timer to Screen

--- JohnLemonMods/3D Beginner/Assets/Scripts/Timer.cs
+++ JohnLemonMods/3D Beginner/Assets/Scripts/Timer.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public float timeRemaining = 10;
     public bool timerIsRunning = false;
     public Text timeText;
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource caughtAudio;
+
+    bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
+    float m_Timer;
+    bool m_HasAudioPlayed;
 
     private void Start()
     {
@@ -29,10 +42,35 @@ public class Timer : MonoBehaviour
                 Debug.Log("Time has run out!");
                 timeRemaining = 0;
                 timerIsRunning = false;
+                EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
+            }
+        }
+    }
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+            
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene (0);
+            }
+            else
+            {
+                Application.Quit ();
             }
         }
     }
 
+    
     void DisplayTime(float timeToDisplay)
     {
         timeToDisplay += 1;
